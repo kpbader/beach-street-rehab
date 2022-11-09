c9c84dd2c6b1e51c138648f65cf75721a4bf6dca
Kyle Patrick Bader <kbader91@gmail.com>
2022-11-08 17:13:54 -0800

    lots added with all pages set up for initial deployment

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,10 +1,47 @@
 import './footer.css';
+import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
 
 function Footer () {
 
-    // return (
-
-    // )
+    return (
+        <section id="footer">
+        {/* <img /> */}
+        <h2>Beach Street Rehab</h2> 
+        <Nav id="footer-links">
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/beach-street-rehab" className="header-nav-tab">Home</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/treatment" className="header-nav-tab">Treatment</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/mission" className="header-nav-tab">Mission</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/about" className="header-nav-tab">About</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/insurance" className="header-nav-tab">Insurance</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/contact" className="header-nav-tab">Contact</Link>
+                </Nav.Link>
+            </Nav.Item>
+        </Nav>
+    </section>
+    )
 
 };
 
