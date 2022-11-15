74157469a2fd9cb826570cfd37d71d730ece61b7
Kyle Patrick Bader <kbader91@gmail.com>
2022-11-15 14:54:16 -0800

    contact info updated

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,7 +1,7 @@
 import './contact.css';
 import Fade from 'react-reveal/Fade';
-// import { BsEnvelope, BsTelephone } from 'react-icons/bs';
-// import { SlLocationPin } from "react-icons/sl";
+import { BsEnvelope, BsTelephone } from 'react-icons/bs';
+import { SlLocationPin } from "react-icons/sl";
 import { useState, useRef } from 'react';
 
 function Contact() {
@@ -13,9 +13,6 @@ function Contact() {
 
     return (
         <section id="contact-page">
-            <Fade up>
-                <p id="contact-page-header">Contact</p>
-            </Fade>
             <div id="contact-container">
                 <Fade left>
                     <form id="contact-form">
@@ -36,8 +33,24 @@ function Contact() {
                     </form>
                 </Fade>
                 <Fade right>
-                    <div id="google-map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13268.063180590256!2d-117.82638843022458!3d33.7602577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dcdbcd13d3be81%3A0x3d8e1cca1625b0c7!2s13961%20Mauve%20Dr%2C%20Santa%20Ana%2C%20CA%2092705!5e0!3m2!1sen!2sus!4v1667494799764!5m2!1sen!2sus" title="map" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <div id="info-and-map">
+                        <div id="contact-info">
+                            <div className="contact-info-line">
+                                <BsEnvelope />
+                                <p>nkhashe@9silver.biz</p>
+                            </div>
+                            <div className="contact-info-line">
+                                <BsTelephone />
+                                <p>(949) 633-1245</p>
+                            </div>
+                            <div className="contact-info-line">
+                                <SlLocationPin />
+                                <p>17682 Beach Blvd. Units 203-204, Huntington Beach, California 92647</p>
+                            </div>
+                        </div>
+                        <div id="google-map">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3319.1282419134686!2d-117.99284860788327!3d33.7056303818455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dd26eebbbc61a1%3A0x905e876fb37add72!2s17682%20Beach%20Blvd%2C%20Huntington%20Beach%2C%20CA%2092647!5e0!3m2!1sen!2sus!4v1668552255488!5m2!1sen!2sus" title="map" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
                     </div>
                 </Fade>
             </div>
