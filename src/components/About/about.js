9ff0a424a04b5a4ebf588762f993c231be3edd8a
Kyle Patrick Bader <kbader91@gmail.com>
2022-12-20 13:03:32 -0800

    updated handleClose for hamburger menu

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -8,11 +8,8 @@ function About() {
         <section id="about-page">
             <div id="about-top">
                 <Fade up>
-                    <div id="about-top-text">
-                        <h2>About Us</h2>
-                        <p>Beach Street Rehab provides a quiet, calm, and safe place to journey towards healing. We believe that in reducing substance abuse and other behavioral health complications among adults by providing addiction treatment services.</p>
-                        <p>We understand each person must be empowered to make the right decisions about their care and improve your quality of life. That's why our team focuses on getting you set up with the right treatment program that works for you.</p>
-                    </div>
+                    <h2>About Us</h2>
+                    <p>Coming soon!</p>
                 </Fade>
             </div>
             <BottomContact />
--- src/components/Insurance/insurance.js
+++ src/components/Insurance/insurance.js
@@ -4,12 +4,7 @@ import anthem from '../../assets/images/Anthem-Inc-logo.png';
 import beacon from '../../assets/images/beacon-logo.png';
 import blueCross from '../../assets/images/bluecross-blueshield-simple-modern-therapy-logo.png';
 import cigna from '../../assets/images/Cigna_logo.svg.png';
-import optum from "../../assets/images/4516741cOptum-logo-ora-RGB.webp"
-import horizon from '../../assets/images/horizon-logo.webp';
 import united from '../../assets/images/United-Healthcare-Logo.png';
-import optima from "../..//assets/images/optima-health-inc-health-care-health-insurance-png-favpng-dumtEeQ0mCtUM1MyRcuN2YYzZ.webp"
-import umr from '../../assets/images/umr-logo.webp';
-import aetna from "../../assets/images/0f0564f1-d965-4e82-9580-862fa8032906.png";
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-router-dom';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
@@ -31,15 +26,10 @@ function Insurance() {
             </div>
             <Fade up>
                 <div id="insurance-logos">
-                <LazyLoadImage src={aetna} alt="logo" />
                     <LazyLoadImage src={anthem} alt="logo" />
                     <LazyLoadImage src={beacon} alt="logo" />
                     <LazyLoadImage src={blueCross} alt="logo" />
                     <LazyLoadImage src={cigna} alt="logo" />
-                    <LazyLoadImage src={horizon} alt="logo" />
-                    <LazyLoadImage src={optima} alt="logo" />
-                    <LazyLoadImage src={optum} alt="logo" />
-                    <LazyLoadImage src={umr} alt="logo" />
                     <LazyLoadImage src={united} alt="logo" />
                 </div>
             </Fade>
